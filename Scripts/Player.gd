d41a277558f3768e777c377dc5c1e5d0ad16727f
boatrocl <sorawittamanunt@gmail.com>
2025-08-31 15:24:31 +0700

    jump

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,18 @@
 extends CharacterBody2D
 
 @export var speed = 150
-@export var jump_force = -300  # แรงกระโดด (ค่าลบเพราะแกน Y ลงเป็นบวก)
-const GRAVITY = 980  # เพิ่ม Gravity ให้เหมาะสม
+@export var jump_force = -300
+const GRAVITY = 980 
 var screen_size 
 signal item_collected(amount)
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
-func _physics_process(delta):  # เปลี่ยนเป็น _physics_process สำหรับการเคลื่อนไหว
-	# Gravity ทำงานตลอดเวลา
+func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	
-	# การเคลื่อนที่แนวนอน
 	var horizontal_input = 0
 	if Input.is_action_pressed("right"):
 		horizontal_input += 1
@@ -23,29 +21,28 @@ func _physics_process(delta):  # เปลี่ยนเป็น _physics_proc
 	
 	velocity.x = horizontal_input * speed
 	
-	# การกระโดด
+	#jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
 	
-	# อัพเดทการเคลื่อนที่
 	move_and_slide()
 	
-	# จำกัดขอบเขตหน้าจอ
+	#screen size
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	# การจัดการ animation
+	#animation
 	if is_on_floor():
 		if abs(velocity.x) > 0:
 			$AnimatedSprite2D.animation = "run"
 			$AnimatedSprite2D.play()
 		else:
-			$AnimatedSprite2D.animation = "idle"  # ควรมี animation idle
+			$AnimatedSprite2D.animation = "idle"
 			$AnimatedSprite2D.play()
 	else:
-		$AnimatedSprite2D.animation = "jump"  # ควรมี animation jump
+		$AnimatedSprite2D.animation = "jump"
 		$AnimatedSprite2D.play()
 	
-	# พลิก Sprite ตามทิศทาง
+	#flip Sprite
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0:
