57d39afa46f5d22dc09a8d98ac6ce4d81c67a285
boatrocl <sorawittamanunt@gmail.com>
2025-09-01 19:17:17 +0700

    keep coin logic

--- Scene/coin.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _ready() -> void:
-	# เริ่มเล่นอนิเมชั่นทันทีเมื่อโหนดพร้อม
-	$AnimatedSprite2D.play("default")
---
+++ Scripts/coin.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+# You can change these to your likings
+@export var amplitude := 4
+@export var frequency := 5
+
+var time_passed = 0
+var initial_position := Vector2.ZERO
+
+func _ready():
+	initial_position = position
+	# เริ่มเล่นอนิเมชั่นทันทีเมื่อโหนดพร้อม
+	$AnimatedSprite2D.play("default")
+
+func _process(delta):
+	coin_hover(delta) # Call the coin_hover function
+
+# Coin Hover Animation
+func coin_hover(delta):
+	time_passed += delta
+	
+	var new_y = initial_position.y + amplitude * sin(frequency * time_passed)
+	position.y = new_y
+
+# Coin collected
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		#AudioManager.coin_pickup_sfx.play()
+		#GameManager.add_score()
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector2.ZERO, 0.1)
+		await tween.finished
+		queue_free()
