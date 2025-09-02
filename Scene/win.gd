168e12ec9ba8cc142bc02511b9b72453bab9b8e5
KorakotPT <korakot.pr@kkumail.com>
2025-09-03 05:50:16 +0700

    VIctoryyyy

---
+++ Scene/win.gd
@@ -0,0 +1,17 @@
+extends Control
+
+#@onready var score_texture = %Score/ScoreTexture
+@onready var score_label = $Panel/Score
+
+func _process(_delta):
+	# Set the score label text to the score variable in game maanger script
+	score_label.text = "Score:%d" % GameManager.score
+
+func _on_menu_pressed() -> void:
+	GameManager.reset_score()
+	get_tree().call_deferred("change_scene_to_file", "res://Scene/Main Menu.tscn")
+
+
+func _on_newgame_pressed() -> void:
+	GameManager.reset_score()
+	get_tree().call_deferred("change_scene_to_file", "res://Scene/Level1.tscn")
