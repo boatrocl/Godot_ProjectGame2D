5e0c6902b8a4fe3593746b5baa6c9576e607fb0f
WaffleXL <jinnawat.05.1991@gmail.com>
2025-09-02 10:45:15 +0700

    Enemy2-Cobra

--- Scripts/enemies_2.gd
+++ Scripts/enemies_2.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+var spd = 100
+var end_path = 0
+
+func _process(delta: float) -> void:
+	$AnimatedSprite2D.play("run")
+	velocity.x= spd 
+	move_and_slide()
+	
+	if position.x == end_path:
+		spd = spd*-1
+	if spd <0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+	
+
+func _on_timer_timeout() -> void:
+	end_path = position.x
