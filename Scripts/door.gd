c9264e10a91b6724e55d5838f6139f11bc735853
Monarizz69 <watcharawit613450@gmail.com>
2025-09-03 00:28:47 +0700

    Door to next scene

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
-@export var next_scene : PackedScene
+@export var next_scene : String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		pass
-		#get_tree().call_deferred("change_scene_to_file", str(next_scene))
+		get_tree().call_deferred("change_scene_to_file", next_scene)
 		#get_tree().change_scene_to_file(str(next_scene))
 		#get_tree().change_scene_to_packed(next_scene)
